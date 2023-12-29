a35f579948d0b0cdd01ca3ec8335ce2b0d2a21eb
Enoch Abiodun <g00380316@atu.ie>
2023-12-29 04:27:37 +0000

    Okay issue fixed import from jpa dependency bug

--- payment/src/main/java/ie/atu/payment/Payment.java
+++ payment/src/main/java/ie/atu/payment/Payment.java
@@ -4,8 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
-import javax.persistence.*;
 import java.time.Instant;
 
 @Entity
