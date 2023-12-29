ce207285e39445072c7ad03ef4293f3ee119bce0
Enoch Abiodun <g00380316@atu.ie>
2023-12-29 04:14:20 +0000

    Controller class logic created but issue

---
+++ payment/src/main/java/ie/atu/payment/PaymentController.java
@@ -0,0 +1,42 @@
+package ie.atu.payment;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ie.atu.payment.payload.PaymentRequest;
+import ie.atu.payment.payload.PaymentResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@RestController
+@RequestMapping("/payment")
+@RequiredArgsConstructor
+@Log4j2
+public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    @PostMapping("")
+    public ResponseEntity<Long> takePayment(@RequestBody PaymentRequest paymentRequest) {
+        log.info("Controller method Called to take Payment");
+        log.info("Payment Request: " + paymentRequest.toString());
+
+        long paymentID = paymentService.proccessPayment(paymentRequest);
+        return new ResponseEntity<>(paymentID, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/order/{orderID}")
+    public ResponseEntity<PaymentResponse> getOrderByID(@PathVariable("id") long orderID) {
+        log.info("Controller method Called to get Order by ID");
+        log.info("Order ID: " + orderID);
+
+        PaymentResponse paymentResponse = paymentService.getPaymentDetailsByOrderID(orderID);
+        return new ResponseEntity<>(paymentResponse, HttpStatus.OK);
+    }
+}
